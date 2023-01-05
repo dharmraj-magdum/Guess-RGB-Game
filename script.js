042e1e357d5c236a216afb45b57e92373b085584
dharmraj-magdum <dkmagdum23@gmail.com>
2023-01-05 21:37:54 +0530

    basic logic done

--- script.js
+++ script.js
@@ -1,60 +1,76 @@
+console.log("Script is running");
 //get elements
 const values = document.getElementById("values");
+const colors = document.getElementById("colors").children;
+const answer = document.getElementById("answer");
 
-var r = Math.floor(Math.random() * 256);
-var g = Math.floor(Math.random() * 256);
-var b = Math.floor(Math.random() * 256);
-
+var corrIdx;
 var difficulty = 5;
-var difficultyValue = 20;
+var difficultyValue = 70;
 var AnsR = 0;
 var AnsG = 0;
 var AnsB = 0;
-function getRandomValues() {
+function setRandomValues() {
 	AnsR = Math.floor(Math.random() * 256);
 	AnsG = Math.floor(Math.random() * 256);
 	AnsB = Math.floor(Math.random() * 256);
 	// return [r, g, b];
 }
-getRandomValues();
-document.getElementById("values").innerHTML =
-	"( " + AnsR + " , " + AnsG + " , " + AnsB + " )";
 
-//select any one ele as answer
-var index = Math.floor(Math.random() * 3);
-
-document.getElementById(`color${index + 1}`).style.backgroundColor =
-	"rgb(" + AnsR + "," + AnsG + "," + AnsB + ")";
-
-//make array of options(expect answer)
-var arr = [];
-for (let i = 0; i < 3; i++) {
-	if (i == index) {
-		continue;
-	}
-	arr.push(i);
-}
 function reset() {
-	for (const i in arr) {
-		var max = AnsR;
-		var r = Math.floor(Math.random() * 256);
-		var max = r + difficultyValue;
-		var min = r - difficultyValue;
-		//get G and B near to R
-		//rand=(0-1*diff)+min gives value rand(max-min)
-		var g = Math.floor(Math.random() * (max - min) + min);
-		var b = Math.floor(Math.random() * (max - min) + min);
-		return [r, g, b];
+	setRandomValues();
+
+	document.getElementById("values").innerHTML =
+		"( " + AnsR + " , " + AnsG + " , " + AnsB + " )";
+
+	//select any one ele as answer
+	corrIdx = Math.floor(Math.random() * 3);
+	console.log(corrIdx);
+	colors[corrIdx].style.backgroundColor =
+		"rgb(" + AnsR + "," + AnsG + "," + AnsB + ")";
+
+	for (let idx = 0; idx < 3; idx++) {
+		if (idx == corrIdx) {
+			continue;
+		}
+		let r = getCloseValues(AnsR);
+		while (r == AnsR) {
+			r = getCloseValues(AnsR);
+		}
+		let g = getCloseValues(AnsG);
+		while (g == AnsG) {
+			g = getCloseValues(AnsG);
+		}
+		let b = getCloseValues(AnsB);
+		while (b == AnsB) {
+			b = getCloseValues(AnsB);
+		}
+		colors[idx].style.backgroundColor =
+			"rgb(" + r + "," + g + "," + b + ")";
 	}
-	//get complete random R
-	var r = Math.floor(Math.random() * 256);
-	var max = r + difficultyValue;
-	var min = r - difficultyValue;
-	//get G and B near to R
+}
+reset();
+//give random value close to give with difficulty
+function getCloseValues(val) {
+	var max = val + difficultyValue;
+	var min = val - difficulty;
+	max = max > 255 ? 255 : max;
+	min = min < 0 ? 0 : min;
 	//rand=(0-1*diff)+min gives value rand(max-min)
-	var g = Math.floor(Math.random() * (max - min) + min);
-	var b = Math.floor(Math.random() * (max - min) + min);
-	return [r, g, b];
+	return Math.floor(Math.random() * (max - min) + min);
 }
 
-function getCloseValues(ch) {}
+function guess() {
+	if (document.activeElement.id - 1 == corrIdx) {
+		answer.innerHTML = "CORRECT !!";
+		answer.style.color = "darkgreen";
+	} else {
+		answer.innerHTML = "WRONG !!";
+		answer.style.color = "red";
+	}
+}
+
+//------------------------------------
+// colors[0].style.backgroundColor = "rgb(" + 255 + "," + 0 + "," + 0 + ")";
+// colors[1].style.backgroundColor = "rgb(" + 0 + "," + 255 + "," + 0 + ")";
+// colors[2].style.backgroundColor = "rgb(" + 0 + "," + 0 + "," + 255 + ")";
